422277290b234622a0e290c2526183ca96b85470
cocutys <syperbeka@gmail.com>
2022-12-20 22:53:30 +0600

    hw2

--- main/settings.py
+++ main/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+from dotenv import load_dotenv
 from pathlib import Path
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&rm%dkakn=(cof0e^0frghgz0l9z^7r_&$0x)w)#x5hhnv9c64'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'book',
+    'tv_show',
 ]
 
 MIDDLEWARE = [
---
+++ tv_show/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+from . import models
+
+def tv_show_view(request):
+    show = models.TvShow.objects.all()
+    return render(request, 'tv_show.html', {'show_object': show})
+
+def tv_show_view_detail(request, id):
+    show_detail = get_object_or_404(models.TvShow, id=id)
+    return render(request, 'tv_show_detail.html', {'object_detail': show_detail})
