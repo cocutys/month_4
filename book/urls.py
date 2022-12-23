31379950d1e63f31aac4032e0e31679a1147d5c7
cocutys <syperbeka@gmail.com>
2022-12-23 23:35:42 +0600

    homework_3

--- book/urls.py
+++ book/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('book/', views.book_view, name='book'),
     path('book_detail/<int:id>/', views.book_view_detail, name='detail'),
+    path('add_book/', views.add_book_view, name='create_book'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from . import models
+from django.http import HttpResponse
+from . import models, forms
 
 def book_view(request):
     book = models.Book.objects.all()
@@ -8,3 +9,14 @@ def book_view(request):
 def book_view_detail(request, id):
     book_detail = get_object_or_404(models.Book, id=id)
     return render(request, 'book_detail.html', {'object_detail': book_detail})
+
+def add_book_view(request):
+    method = request.method
+    if method == 'POST':
+        form = forms.BookForm(request.POST, request.FILES)
+        form.save()
+        return HttpResponse('<h1>Книга успешно добавлена</h1>')
+    else:
+        form = forms.BookForm()
+
+    return render(request, 'create_book.html', {'form': form})
